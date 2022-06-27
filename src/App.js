ff98ecda284883758d4687295a1a8292c2eaf143
Olha Pushkar <Olha_Pushkar@epam.com>
2022-06-27 22:47:50 +0300

    init project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+     <form>
+      <div className='mb-3'>
+        <label htmlFor='password'>
+          Password
+        </label>
+        <input
+          type='password'
+          id='password'
+          name='password'
+        />
+      </div>
+      <div className='mb-3'>
+        <label htmlFor='confirm-password'>
+          Confirm password
+        </label>
+        <input
+          type='password'
+          id='confirm-password'
+          name='confirm-password'
+          className='form-control'
+        />
+      </div>
+      <div className='mb-3'>
+        <label htmlFor='email'>
+          email
+        </label>
+        <input
+          type='email'
+          id='email'
+          name='email'
+        />
+      </div>
+     </form>
     </div>
   );
 }
